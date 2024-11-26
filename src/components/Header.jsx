c97bc851a0d05dcd458e8b5f8ff61d919e2056ca
jocnn <ijann_@outlook.com>
2024-11-26 11:27:50 -0300

    Applying useMemo for performance of the cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
+import { useMemo } from "react"
+
 export default function Header({ cart }) {
-  const isEmpty = () => cart.length === 0
-  const cartTotal = () => cart.reduce((total, item) => total + (item.quantity*item.price), 0)
+  const isEmpty = useMemo(() => cart.length === 0, [cart])
+  const cartTotal = useMemo(
+    () => cart.reduce((total, item) => total + item.quantity * item.price, 0),
+    [cart]
+  )
 
   return (
     <header className="py-5 header">
@@ -24,7 +29,7 @@ export default function Header({ cart }) {
               />
 
               <div id="carrito" className="bg-white p-3">
-                {isEmpty() ? (
+                {isEmpty ? (
                   <p className="text-center">El carrito esta vacio</p>
                 ) : (
                   <>
@@ -69,7 +74,8 @@ export default function Header({ cart }) {
                       </tbody>
                     </table>
                     <p className="text-end">
-                      Total pagar: <span className="fw-bold">${cartTotal()}</span>
+                      Total pagar:{" "}
+                      <span className="fw-bold">${cartTotal}</span>
                     </p>
                   </>
                 )}
